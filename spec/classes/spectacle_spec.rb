fe68868ec67cd22fac8f3d4b5978918e1ad2f781
Salimane Adjao Moustapha <me@salimane.com>
2014-12-06 00:57:16 +0100

    add version parameter

--- spec/classes/spectacle_spec.rb
+++ spec/classes/spectacle_spec.rb
@@ -6,10 +6,11 @@ describe 'spectacle' do
       :boxen_home => '/opt/boxen'
     }
   end
+  let(:params) { {:version => '0.8.8'} }
 
   it do
     should contain_package('spectacle').with({
-      :source   => 'https://s3.amazonaws.com/spectacle/downloads/Spectacle+0.8.2.zip',
+      :source   => 'https://s3.amazonaws.com/spectacle/downloads/Spectacle+0.8.8.zip',
       :provider => 'compressed_app'
     })
   end
